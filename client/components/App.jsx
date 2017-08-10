d2f8dd98ed5dc8c9e55151ab9ef8d9cd0421d3df
Nathan Lamb <nathanlamb022@gmail.com>
2017-08-11 09:59:58 +1200

    Remove word components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 
-import AddWord from '../containers/AddWord'
-import Words from '../containers/Words'
 
 const App = () => (
   <div className='app-container'>
-    <Words />
-    <AddWord />
   </div>
 )
 
